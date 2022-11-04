5ddd31617861672322f03de199cbdc36e8fc5d64
Kai Helli <dpd3zd@virginia.edu>
2022-11-04 03:37:16 -0400

    Add basic register form to the frontend.

--- client/ui/src/app/auth/signup/signup.component.ts
+++ client/ui/src/app/auth/signup/signup.component.ts
@@ -1,15 +1,58 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthService } from "../../services/auth.service";
+
+/*type User = {
+  username: string;
+  roles: string[];
+};*/
 
 @Component({
-  selector: 'app-signup',
+  selector: 'app-login',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.scss']
 })
 export class SignupComponent implements OnInit {
+  public signupValid = true;
+  public username = '';
+  public password = '';
+  public passwordConfirm = '';
+  // TODO: minLowercase: 1, minUppercase: 1, minNumbers: 0, minSymbols: 1, not same as username
+  // TODO: Display errors and maybe verify input (will be done by backend aswell though).
+  public errorMessage = '';
 
-  constructor() { }
+  private readonly returnUrl: string;
 
-  ngOnInit(): void {
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthService
+  ) {
+    //this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/dashboard';
+    this.returnUrl = '/dashboard';
+  }
+
+  public ngOnInit(): void {
+    this.authService.isSignedIn().subscribe(value => {
+      if (value) {
+        this.router.navigateByUrl(this.returnUrl);
+      }
+    });
+  }
+
+  public onSubmit(): void {
+    this.signupValid = true;
+
+    this.authService.signup(this.username, this.password).subscribe({
+      next: _ => {
+        this.signupValid = true;
+        this.router.navigateByUrl(this.returnUrl);
+      },
+      error: err => {
+        this.errorMessage = err.message;
+        this.signupValid = false;
+      }
+    });
   }
 
 }
