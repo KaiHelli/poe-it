99c3c4ae0c27958c5172dc13fd6efb071d799b32
Kai Helli <dpd3zd@virginia.edu>
2022-11-29 23:52:40 -0500

    Fix account route.

--- client/ui/src/app/auth/auth-routing.module.ts
+++ client/ui/src/app/auth/auth-routing.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import {inject, NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
 import { SignupComponent } from './signup/signup.component';
@@ -9,7 +9,7 @@ import { AuthService } from "../services/auth.service";
 const routes: Routes = [
   { path: 'signin', component: SigninComponent },
   { path: 'signup', component: SignupComponent },
-  { path: 'account', component: AccountComponent, canActivate: [AuthService]}
+  { path: 'account', component: AccountComponent, canActivate: [() => inject(AuthService).forceSignedIn()]}
 ];
 
 @NgModule({
